b3a71d0002f5c087a029ba63cb60a83b71f0ca8d
Sntsjrg <sntsjrg@yahoo.com>
2024-01-29 21:09:17 -0500

    new

---
+++ Starter_Code/static/js/logic.js
@@ -0,0 +1,75 @@
+ // Define earthquakes plates GeoJSON url variable
+ const geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+ let geojson;
+ //set map
+ const myMap = L.map("map", {
+     center: [
+         37.09, -95.71
+     ],
+     zoom: 5,
+ });
+ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+ }).addTo(myMap);
+
+ //retreive data
+ d3.json(geoData).then(function (data) {
+     function mapStyle(feature) {
+         return {
+             opacity: 1,
+             fillOpacity: 1,
+             fillColor: mapColor(feature.geometry.coordinates[2]),
+             color: "#000000",
+             radius: mapRadius(feature.properties.mag),
+             stroke: true,
+             weight: 0.5
+         
+         };
+    
+// set depth colors
+     }
+     function mapColor(depth) {
+         switch (true) {
+             case depth > 90:
+                 return "red";
+             case depth > 70:
+                 return "orangered";
+             case depth > 50:
+                 return "orange";
+             case depth > 30:
+                 return "gold";
+             case depth > 10:
+                 return "yellow";
+             default:
+                 return "lightgreen";
+         }
+     }
+// set magnitude size
+     function mapRadius(mag) {
+         if (mag === 0) {
+             return 1;
+         }
+         return mag * 4;
+     }
+     L.geoJson(data, {
+         pointToLayer: function (feature, latlng) {
+             return L.circleMarker(latlng);
+         },
+         style: mapStyle,
+         onEachFeature: function (feature, layer) {
+             layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Depth: " + feature.geometry.coordinates[2]);
+         }
+     }).addTo(myMap);
+
+// Add legend
+const legend = L.control({ position: "topleft" });
+legend.onAdd = function() {
+    const div = L.DomUtil.create("div", "info legend");
+    const depth = [-10, 10, 30, 50, 70, 90];
+    for (let i = 0; i < depth.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + mapColor(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
+legend.addTo(myMap)});
